6d6a8218f2e810fb5d56c74ab8a6e0fdc7017180
gerdajj <gerda.jae@ut.ee>
2025-10-06 14:24:02 +0300

    add type converters for Date

--- app/src/main/java/com/example/evntly/data/EventDatabase.kt
+++ app/src/main/java/com/example/evntly/data/EventDatabase.kt
@@ -2,8 +2,10 @@ package com.example.evntly.data
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 
 @Database(entities = [Event::class], version = 1)
+@TypeConverters(Converters::class)
 abstract class EventDatabase : RoomDatabase() {
     abstract fun getEventDao(): EventDao
 }
