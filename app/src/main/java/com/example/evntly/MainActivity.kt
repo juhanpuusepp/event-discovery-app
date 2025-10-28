7c6b6f46f63c4f70bd64763ac641c92dd8259deb
gerdajj <gerda.jae@ut.ee>
2025-10-28 23:09:24 +0200

    removed hardcoded event

--- app/src/main/java/com/example/evntly/MainActivity.kt
+++ app/src/main/java/com/example/evntly/MainActivity.kt
@@ -5,17 +5,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.lifecycle.lifecycleScope
 import androidx.room.Room
 import com.example.evntly.data.local.EventDatabase
 import com.example.evntly.ui.navigation.AppNavHost
 import com.example.evntly.data.repository.EventRepository
-import com.example.evntly.domain.model.Event
 import com.example.evntly.ui.theme.EvntlyTheme
 import com.example.evntly.ui.viewmodel.EventViewModel
 import com.example.evntly.ui.viewmodel.EventViewModelFactory
-import kotlinx.coroutines.launch
-import java.util.Date
 import kotlin.getValue
 
 class MainActivity : ComponentActivity() {
@@ -32,20 +28,6 @@ class MainActivity : ComponentActivity() {
 
         val viewModel: EventViewModel by viewModels {
             EventViewModelFactory(repository)
-        }
-
-        // temporary hardcoded event
-        lifecycleScope.launch {
-            val sampleEvent = Event(
-                name = "Test Concert",
-                date = Date(),
-                price = 15.0,
-                description = "Sample event for testing map markers",
-                location = "Tartu Town Hall Square",
-                latitude = 58.3780,   // Tartu Town Hall
-                longitude = 26.7225
-            )
-            viewModel.addEvent(sampleEvent)
         }
 
         installSplashScreen()
